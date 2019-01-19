1b818cbc2acc83709911b5862d84f28258222313
korynewton <korynewtondev@gmail.com>
2019-01-19 10:18:02 -0800

    array stretches

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,10 +74,17 @@ for (let i=0; i<inventory.length; i++) {
     }
 }
 
+//stretch:
+const filterID = inventory.filter(car => car['id'] === 33)
+console.log(filterID[0])
+
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = `Make: ${inventory[inventory.length - 1].car_make} Model: ${inventory[inventory.length - 1].car_model}`;
 console.log(lastCar);
+
+
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -87,6 +94,8 @@ for(let i=0; i<inventory.length; i++) {
 }
 console.log(carModels.sort());
 
+
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
@@ -94,6 +103,8 @@ for(let i=0; i<inventory.length; i++) {
     carYears.push(inventory[i]['car_year'])  
   }
 console.log(carYears);
+
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -105,6 +116,10 @@ for (let i=0; i<carYears.length; i++) {
 }
 console.log(oldCars.length);
 
+// Stretch:
+const oldCarsFilter = inventory.filter(old => old.car_year < 2000)
+console.log(oldCarsFilter.length)
+
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
@@ -114,3 +129,7 @@ for (let i=0; i<inventory.length; i++) {
     }
 }
 console.log(JSON.stringify(BMWAndAudi));
+
+//stretch:
+const BMWandAudiFilter = inventory.filter(nice_cars => nice_cars['car_make'] === 'Audi' | nice_cars['car_make'] === 'BMW')
+console.log(BMWandAudiFilter)
